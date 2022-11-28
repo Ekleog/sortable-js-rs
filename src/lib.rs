d3509931a9116dc6e69f6b90bb2ff115c8100dd4
Léo Gaspard <leo@gaspard.io>
2022-11-28 18:56:00 +0100

    fix setting options

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,37 @@ mod js {
         pub type Sortable;
 
         #[wasm_bindgen(constructor)]
-        pub fn new(elt: &web_sys::Element, opts: &js_sys::Map) -> Sortable;
+        pub fn new(elt: &web_sys::Element, opts: &js_sys::Object) -> Sortable;
     }
 }
 
-pub struct Options(js_sys::Map);
+/// See https://github.com/SortableJS/Sortable for more documentation about available options
+pub struct Options(js_sys::Object);
+
+macro_rules! option {
+    ( $setter:ident, $jsname:expr, $typ:ty, $builder:ident ) => {
+        pub fn $setter(&self, value: $typ) -> &Options {
+            let res = js_sys::Reflect::set(
+                &self.0,
+                &JsValue::from_str($jsname),
+                &JsValue::$builder(value)
+            ).expect("setting property on object failed");
+            assert!(res, "failed setting property on object");
+            self
+        }
+    }
+}
 
 impl Options {
     pub fn new() -> Options {
-        Options(js_sys::Map::new())
+        Options(js_sys::Object::new())
     }
 
-    pub fn set_group(&self, group: &str) -> &Options {
-        self.0.set(&JsValue::from_str("group"), &JsValue::from_str(group));
-        self
-    }
+    option!(set_group, "group", &str, from_str);
+    option!(set_sort, "sort", bool, from_bool);
+    option!(set_delay, "delay", f64, from_f64);
+    option!(set_disabled, "disabled", bool, from_bool);
+    option!(set_handle, "handle", &str, from_str);
 
     pub fn apply(&self, elt: &web_sys::Element) {
         js::Sortable::new(elt, &self.0);
